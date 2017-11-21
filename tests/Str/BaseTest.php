0e1f75c7fc06bcfa42ffec23c3bf4339bf0916d5
xiaolin <462441355@qq.com>
2017-11-21 15:41:10 +0800

    * 初始化

---
+++ tests/Str/BaseTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Tests\Str;
+// +----------------------------------------------------------------------
+// | BaseTest.php [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016-2017 xiaolin All rights reserved.
+// +----------------------------------------------------------------------
+// | Author: limx <462441355@qq.com> <https://github.com/missxiaolin>
+// +----------------------------------------------------------------------
+
+use PHPUnit\Framework\TestCase;
+
+class BaseTest extends TestCase
+{
+    public function testWrap()
+    {
+        $this->assertTrue(true);
+    }
+}
