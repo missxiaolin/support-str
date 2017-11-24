922301962c6b38167b5090df6f46504ec5d88847
xiaolin <462441355@qq.com>
2017-11-24 09:37:36 +0800

    * edit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 // +----------------------------------------------------------------------
-// | BaseTest.php [ WE CAN DO IT JUST THINK IT ]
+// | bootstrap.php [ WE CAN DO IT JUST THINK IT ]
 // +----------------------------------------------------------------------
 // | Copyright (c) 2016-2017 xiaolin All rights reserved.
 // +----------------------------------------------------------------------
-// | Author: limx <462441355@qq.com> <https://github.com/missxiaolin>
+// | Author: xiaolin <462441355@qq.com> <https://github.com/missxiaolin>
 // +----------------------------------------------------------------------
 require __DIR__ . '/../vendor/autoload.php';
